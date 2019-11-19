ee92c2113d4b2b6112c4c2f849e136aa39fc3974
Esaomr <1835601254@qq.com>
2019-11-19 14:47:33 +0800

    修改

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -3,30 +3,67 @@ package com.example.demo.controller;
 import com.example.demo.entity.CustomUser;
 import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RequestMapping("/")
+@Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    //查询用户信息
+    /**
+     * 一般页面跳转
+     */
+    @RequestMapping(value = "/User/{page}", method = RequestMethod.GET)
+    public String UserPageSkip(@PathVariable("page") String page){
+        return page;
+    }
+
+    /**
+     * 查询用户信息
+     * @param customUser user拓展类对象
+     */
     @RequestMapping(value = "/User", method = RequestMethod.GET)
+    @ResponseBody
     public List<CustomUser> SelectUser(CustomUser customUser){
         List<CustomUser> users = userService.SelectUser(customUser);
         return users;
     }
 
-    //新增用户信息
+    /**
+     * 新增用户信息
+     * @param customUser user拓展类对象
+     */
     @RequestMapping(value = "/User", method = RequestMethod.POST)
+    @ResponseBody
     public List<CustomUser> InsertUser(CustomUser customUser){
         List<CustomUser> users = userService.InsertUser(customUser);
+        return users;
+    }
+
+    /**
+     * 修改用户信息
+     * @param customUser user拓展类对象
+     */
+    @RequestMapping(value = "/User", method = RequestMethod.PUT)
+    @ResponseBody
+    public List<CustomUser> UpdateUser(CustomUser customUser){
+        List<CustomUser> users = userService.UpdateUser(customUser);
+        return users;
+    }
+
+    /**
+     * 删除用户信息
+     * @param customUser user拓展类对象
+     */
+    @RequestMapping(value = "/User", method = RequestMethod.DELETE)
+    @ResponseBody
+    public List<CustomUser> DeleteUser(CustomUser customUser){
+        List<CustomUser> users = userService.DeleteUser(customUser);
         return users;
     }
 
--- demo/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.demo.service;
 import com.example.demo.entity.CustomUser;
 import com.example.demo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
